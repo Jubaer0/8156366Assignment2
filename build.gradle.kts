b00e2c601d310936fcf8055b9b520aaafbdfb022
Jubaer0 <138960777+Jubaer0@users.noreply.github.com>
2026-09-15 09:39:44 +1000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "9.3.2" apply false
+    id("com.android.application") version "9.2.1" apply false
     id("com.google.devtools.ksp") version "2.3.6" apply false
     id("com.google.dagger.hilt.android") version "2.59.2" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.9.7" apply false
